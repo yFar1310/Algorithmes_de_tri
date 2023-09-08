06255877fd033162f797e490bf8b3641b822b4be
Farehan Yahya <lazytotest@gmail.com>
2023-09-08 22:49:04 +0200

    All good

---
+++ Python/Tri_selection.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct  4 17:44:40 2022
+
+@author: Farehan Yahya
+"""
+
+def tri_par_selection(T):
+    n = len(T)
+    for i in range(n-1):
+        min = T[i]
+        for j in range(i+1,n):
+            if T[j]<min:
+                min  = j
+                T[i],T[min]=T[min],T[i] 
+    return T
+                
+            
+            
+L=[2,3,8,11,14,13,65,23,65,85,462,25,98,114]
+print(tri_par_selection(L))
