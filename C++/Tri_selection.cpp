c6167f9fc94783079e84fab0da489fe06dc108f6
Farehan Yahya <lazytotest@gmail.com>
2023-09-09 22:18:16 +0200

    Tri par sélection en C++

---
+++ C++/Tri_selection.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+void swap(int &a, int &b) {
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+void selectionSort(int arr[], int size) {
+    for (int i = 0; i < size - 1; i++) {
+        // Trouver l'indice du minimum dans le tableau non trié
+        int minIndex = i;
+        for (int j = i + 1; j < size; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+
+        // Échanger l'élément minimum avec l'élément à la position i
+        swap(arr[i], arr[minIndex]);
+    }
+}
+
+int main() {
+    int arr[] = {64, 25, 12, 22, 11};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    std::cout << "Tableau avant le tri par sélection : ";
+    for (int i = 0; i < size; i++) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+
+    selectionSort(arr, size);
+
+    std::cout << "Tableau après le tri par sélection : ";
+    for (int i = 0; i < size; i++) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
