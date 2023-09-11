25135f6d70172d1e197973a67db5f7cd3e4b894f
Farehan Yahya <lazytotest@gmail.com>
2023-09-11 21:58:28 +0200

    UPDATE

---
+++ C++/Tri_insertion.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+
+void insertionSort(std::vector<int>& arr) {
+    int n = arr.size();
+    for (int i = 1; i < n; i++) {
+        int key = arr[i];
+        int j = i - 1;
+        
+        // Déplacer les éléments de arr[0..i-1] qui sont plus grands que key
+        // vers la droite d'une position.
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        
+        arr[j + 1] = key;
+    }
+}
+
+int main() {
+    std::vector<int> arr = {12, 11, 13, 5, 6};
+    
+    std::cout << "Tableau non trié : ";
+    for (int val : arr) {
+        std::cout << val << " ";
+    }
+    std::cout << std::endl;
+    
+    insertionSort(arr);
+    
+    std::cout << "Tableau trié : ";
+    for (int val : arr) {
+        std::cout << val << " ";
+    }
+    std::cout << std::endl;
+    
+    return 0;
+}
